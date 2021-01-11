e1c90fe41abeef4e29346c69d2865a1d571399cd
kencormican <ken.cormican@bt.com>
2021-01-11 11:49:53 +0000

    Stripe Lesson Code 2 Added. Stripe Elements Added

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+         'stripe_public_key': 'pk_test_51I896EKt3nd5V9dl3nqH1s5E0tSduGijapjDcN6o3YfLnAUpDq7fGndyCK7HlJ270mn3raSGyUZny4NycLtRFgez00cwWOfiYa',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
