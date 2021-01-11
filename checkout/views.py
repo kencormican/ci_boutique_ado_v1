9ae592dc17a5d0dd89553cf291215b9643288b5b
kencormican <ken.cormican@bt.com>
2021-01-11 13:11:32 +0000

    Stripe Lesson Code 3 Added. Env variables set & Card Event Listener added

--- checkout/views.py
+++ checkout/views.py
@@ -3,12 +3,20 @@ from django.contrib import messages
 
 from .forms import OrderForm
 
+from django.conf import settings
+from bag.contexts import bag_contents
+
+import stripe
 
 def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
